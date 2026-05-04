5517b15238cfdaaa9b3c9509de4a694095ceff47
TCañete-10R <canete.tc@obmontessori.edu.ph>
2026-05-04 09:27:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,33 @@ import logging
 logging.getLogger('matplotlib').setLevel(logging.ERROR)
 import matplotlib.pyplot as plt
 
+class Classmate:
+    def __init__(self, name, section, subject):
+        self.name = name
+        self.section = section
+        self.subject = subject
+
+    def introduce(self):
+        display(f"Hi! I am {self.name} from {self.section}. My favorite subject is {self.subject}.", target="output")
+
+classmates = [
+    Classmate("Enzo", "Ruby", "Math"),
+    Classmate("Koby", "Ruby", "Science")
+]
+
+def add_classmate(e):
+    name = document.getElementById("name").value
+    section = document.getElementById("section").value
+    subject = document.getElementById("subject").value
+
+    classmates.append(Classmate(name, section, subject))
+    display("Added!", target="output")
+
+def show_list(e):
+    document.getElementById("output").innerHTML = ""
+    for cm in classmates:
+        cm.introduce()
+
 # Initial data
 days = np.array(['Mon','Tue','Wed','Thu','Fri'])
 sales = np.array([1, 0, 7, 4, 9])  # starting absences
